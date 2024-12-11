0e7a5e41e56a78c3c39d1988ced036b485f5e791
paul <paul@fulcrumpro.com>
2024-12-11 04:44:14 -0600

    Remove test code

--- cs/Day11/Solver.cs
+++ cs/Day11/Solver.cs
@@ -38,40 +38,8 @@ public class Solver(string input)
             }
 
             stones = newStones;
-            // Console.WriteLine($"After {blink + 1} blinks");
-            // foreach (var (stone, count) in stones)
-            // {
-            //     Console.WriteLine($"stone {stone} appears {count} times");
-            // }
-            // Console.WriteLine();
         }
 
         return stones.Values.Sum();
-        
-        // while (stack.TryPop(out var node))
-        // {
-        //     var (val, left) = node;
-        //     if (left == 0)
-        //     {
-        //         res++;
-        //         continue;
-        //     }
-
-        //     var s = val.ToString();
-        //     if (val == 0)
-        //     {
-        //         stack.Push((1, left - 1));
-        //     }
-        //     else if (s.Length % 2 == 0)
-        //     {
-        //         stack.Push((long.Parse(s[0..(s.Length / 2)]), left - 1));
-        //         stack.Push((long.Parse(s[(s.Length / 2)..]), left - 1));
-        //     }
-        //     else
-        //     {
-        //         stack.Push((2048, left - 1));
-        //     }
-        // }
-        // return res;
     }
 }
